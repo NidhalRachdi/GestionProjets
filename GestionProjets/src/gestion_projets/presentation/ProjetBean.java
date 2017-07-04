1021dd8c2d514c146631aeb611b393c21dbf0394
NidhalRachdi <rachdi.nidhal@gmail.com>
2017-07-04 15:49:29 +0200

    Code optimization-- --Exit the method that retrieves the parm of the url to a new package --Instantiate the object to retrieve the parameters of the url

--- GestionProjets/src/gestion_projets/presentation/ProjetBean.java
+++ GestionProjets/src/gestion_projets/presentation/ProjetBean.java
@@ -1,17 +1,13 @@
 package gestion_projets.presentation;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.annotation.Resources;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.component.FacesComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
@@ -32,255 +28,227 @@ import gestion_projets.services.TypeServicesImpl;
  *
  */
 
-@ManagedBean(name="projetBean1")
+@ManagedBean(name = "projetBean1")
 
 @RequestScoped
 
-
 public class ProjetBean {
 
-	//Add logger fo display title
-	public Logger log= Logger.getLogger(ProjetBean.class);
-	
-	//appelle couche services 
-	
+	// Add logger fo display title
+	public Logger log = Logger.getLogger(ProjetBean.class);
+
+	// appelle couche services
+
 	private ProjetServices projetServices = new ProjetServicesImpl();
 	private TypeServices typeServices = new TypeServicesImpl();
-	
-	
-	
-	
+
 	private String title;
 	private String description;
 	private String budget;
 	private String type;
 	private String active;
-	
+
 	private String success;
-	private boolean showForm=true;
-	//chaque objet dans cette liste est une ligne dans le menu de type
+	private boolean showForm = true;
+	// chaque objet dans cette liste est une ligne dans le menu de type
 	private List<SelectItem> typeList;
 	private List<Projet> projetList;
-	
+
 	private String id;
 	private String operation;
-	
+
 	{
-		
+
 		System.out.println("Block !");
 	}
-	
+
 	static {
-		
+
 		System.out.println("Block static !");
 	}
-	
+
 	public ProjetBean() {
 		System.out.println("construct!");
 	}
-	
-	//Allow this function to be applied first
-		//on initialise les objets dans cette méthode
-	
-	
-	
-	
-	
-		@SuppressWarnings("unused")
-		@PostConstruct
-		public void initBean(){
-			
-			System.out.println("Post construct !");
-			//showForm=false;
-			
-			//remplir typeList
-			typeList =new ArrayList<SelectItem>();
-			typeList.add(new SelectItem("", ""));
-			//typeList.add(new SelectItem(1, "Informatique"));
-			//typeList.add(new SelectItem(2, "Commerce"));
-			//typeList.add(new SelectItem(3, "Autre"));
-			try{
+
+	// Allow this function to be applied first
+	// on initialise les objets dans cette méthode
+
+	@SuppressWarnings("unused")
+	@PostConstruct
+	public void initBean() {
+
+		System.out.println("Post construct !");
+		// showForm=false;
+
+		// remplir typeList
+		typeList = new ArrayList<SelectItem>();
+		typeList.add(new SelectItem("", ""));
+		// typeList.add(new SelectItem(1, "Informatique"));
+		// typeList.add(new SelectItem(2, "Commerce"));
+		// typeList.add(new SelectItem(3, "Autre"));
+		try {
 			List<Type> listServices = typeServices.findAll();
-			for(Type o:listServices){
+			for (Type o : listServices) {
 				typeList.add(new SelectItem(o.getId(), o.getName()));
 			}
-			}catch(Exception e){
-				
-				System.out.println("Exception 2 à gérée");
-			}
-			//remplir projetList
-			
-			projetList = projetServices.findAll();
-			
-			
-			System.out.println("id : " + getParam("id"));
-			System.out.println("operation : " + getParam("operation"));
-			
-			if("edit".equalsIgnoreCase(getParam("operation"))){
-				setOperation(getParam("operation"));
-				Long id =null;
-				Projet projet=null;
-				
-				try{
-					id=Long.valueOf(getParam("id"));
-					setId(getParam("id"));
-				}catch(Exception e){
-				}
-				
-				
-				if(id !=null){
-					
-					projet = projetServices.findById(id);
-					
-					if(projet !=null){
-						
-						title=projet.getTitle();
-						description=projet.getDescription();
-						
-						
-						if(projet.getBudget() !=null){
-						budget=projet.getBudget().toString();
-						}
-						
-						
-						type=projet.getTypeId().toString()+ "";
-						active=projet.getActive();	
-						
-						showForm=true;
-					}
-				}
-				 
-				
-				
-				
-			}
-		}
-		
-		public String getParam(String name){
-			
-			
-			FacesContext fc = FacesContext.getCurrentInstance();
-			
-			Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
-			
-			return params.get(name);
-		}
-		
-		
-		
-		
-	public void showFormAction(ActionEvent e){
-			
-			log.info("true");
-			showForm=true;
-		}
-		
-		
-	public void cancelAction(ActionEvent e){
-			
-			log.info("false");
-			showForm=false;
-		}
+		} catch (Exception e) {
 
-	public void generateDescription(ActionEvent e){
-		
-		String desc = "";
-		
-		desc+="Le titre est :" + title;
-		desc= desc + "Le montant : " + budget ;
-		desc+="Active :" + ("Y".equalsIgnoreCase(active)?"Oui" : "Non");
-		description=desc;
-		
-		title="";
-		budget="";
-		active="";
-		
-	}
-	public void addProject(ActionEvent e){
-		
-		success="";
-		
-		if("".equalsIgnoreCase(title)){
-			
-			
-			FacesContext.getCurrentInstance().addMessage("title", new FacesMessage("Le titre est vide ! "));
-			
-		}else if(title.length()>10){
-			FacesContext.getCurrentInstance().addMessage("title", new FacesMessage("Le titre est supp de 10 chars ! "));
+			System.out.println("Exception 2 à gérée");
 		}
-		else{
-			
-			
+		// remplir projetList
+
+		projetList = projetServices.findAll();
+
+		System.out.println("id : " + getParam("id"));
+		System.out.println("operation : " + getParam("operation"));
+
+		if ("edit".equalsIgnoreCase(getParam("operation"))) {
+			setOperation(getParam("operation"));
+			Long id = null;
+			Projet projet = null;
+
+			try {
+				id = Long.valueOf(getParam("id"));
+				setId(getParam("id"));
+			} catch (Exception e) {
+			}
+
+			if (id != null) {
+
+				projet = projetServices.findById(id);
+
+				if (projet != null) {
+
+					title = projet.getTitle();
+					description = projet.getDescription();
+
+					if (projet.getBudget() != null) {
+						budget = projet.getBudget().toString();
+					}
+
+					type = projet.getTypeId().toString() + "";
+					active = projet.getActive();
+
+					showForm = true;
+				}
+			}
+
+		}
+	}
+
+	public String getParam(String name) {
+
+		FacesContext fc = FacesContext.getCurrentInstance();
+
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+
+		return params.get(name);
+	}
+
+	public void showFormAction(ActionEvent e) {
+
+		log.info("true");
+		showForm = true;
+	}
+
+	public void cancelAction(ActionEvent e) {
+
+		log.info("false");
+		showForm = false;
+	}
+
+	public void generateDescription(ActionEvent e) {
+
+		String desc = "";
+
+		desc += "Le titre est :" + title;
+		desc = desc + "Le montant : " + budget;
+		desc += "Active :" + ("Y".equalsIgnoreCase(active) ? "Oui" : "Non");
+		description = desc;
+
+		title = "";
+		budget = "";
+		active = "";
+
+	}
+
+	public void addProject(ActionEvent e) {
+
+		success = "";
+
+		if ("".equalsIgnoreCase(title)) {
+
+			FacesContext.getCurrentInstance().addMessage("title", new FacesMessage("Le titre est vide ! "));
+
+		} else if (title.length() > 10) {
+			FacesContext.getCurrentInstance().addMessage("title", new FacesMessage("Le titre est supp de 10 chars ! "));
+		} else {
+
 			log.error("Les valeurs : ");
-			
-			log.info(" titre : "+ title);
-			log.info(" description : "+ description);
-			log.info(" budget : "+ budget);
-			log.info(" type : "+ type);
-			log.info(" active : "+ active);
-			
-			Projet p =null;
-			
+
+			log.info(" titre : " + title);
+			log.info(" description : " + description);
+			log.info(" budget : " + budget);
+			log.info(" type : " + type);
+			log.info(" active : " + active);
+
+			Projet p = null;
+
 			System.out.println("addProject : " + operation + "-" + id);
-			
-			if("edit".equalsIgnoreCase(operation)){
-				
+
+			if ("edit".equalsIgnoreCase(operation)) {
+
 				p = projetServices.findById(new Long(id));
 				System.out.println("edit");
-			}else{
-				
-				p = new Projet(); 
+			} else {
+
+				p = new Projet();
 				System.out.println("new");
 			}
-			
+
 			p.setTitle(title);
 			p.setDescription(description);
 			p.setBudget(Double.valueOf(budget));
 			p.setTypeId(Long.valueOf(type));
 			p.setActive(active);
-			
+
 			projetServices.add(p);
-			
-			success="Bien ajouté";
-			
-			title="";
-			description="";
-			budget="";
-			type="";
-			active="";
-			id ="";
-			operation="";
+			success = "Bien ajouté";
+
+			title = "";
+			description = "";
+			budget = "";
+			type = "";
+			active = "";
+			id = "";
+			operation = "";
 			// add dataBase
 		}
-		
+
 	}
-	public void deleteProject(ActionEvent e){
-		
+
+	public void deleteProject(ActionEvent e) {
+
 		log.info("delete project");
-		
-		//id et name
-		
+
+		// id et name
+
 		FacesContext fc = FacesContext.getCurrentInstance();
 		Map<String, String> param = fc.getExternalContext().getRequestParameterMap();
-		
+
 		log.info("id :" + param.get("id"));
 		log.info("name :" + param.get("name"));
 		projetServices.delete(new Long(param.get("id")));
 		projetList = projetServices.findAll();
 	}
-	
-	
-	
-	
-	
-	//Ajout methode qui affiche le message tappé par l'utilisateur
-	public void saveDate(ActionEvent e){
+
+	// Ajout methode qui affiche le message tappé par l'utilisateur
+	public void saveDate(ActionEvent e) {
 		// Affichage avec log
-		log.info("Le titre est : "+ title);
-		
+		log.info("Le titre est : " + title);
+
 	}
-	
-	
 
 	public String getTitle() {
 		return title;
@@ -289,8 +257,7 @@ public class ProjetBean {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	
+
 	public String getDescription() {
 		return description;
 	}
@@ -299,8 +266,6 @@ public class ProjetBean {
 		this.description = description;
 	}
 
-	
-	
 	public String getBudget() {
 		return budget;
 	}
@@ -308,8 +273,7 @@ public class ProjetBean {
 	public void setBudget(String budget) {
 		this.budget = budget;
 	}
-	
-	
+
 	public String getType() {
 		return type;
 	}
@@ -318,21 +282,17 @@ public class ProjetBean {
 		this.type = type;
 	}
 
-
 	public List<SelectItem> getTypeList() {
 		return typeList;
 	}
-
 
 	public void setTypeList(List<SelectItem> typeList) {
 		this.typeList = typeList;
 	}
 
-
 	public String getActive() {
 		return active;
 	}
-
 
 	public void setActive(String active) {
 		this.active = active;
@@ -401,9 +361,5 @@ public class ProjetBean {
 	public void setTypeServices(TypeServices typeServices) {
 		this.typeServices = typeServices;
 	}
-	
-	
-	
-	
-	
+
 }
